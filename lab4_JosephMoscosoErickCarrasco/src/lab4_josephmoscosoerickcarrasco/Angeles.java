c090cd29c4871a12b774b658c25fa48a808169bd
WX <erick.e.carrasco88@gmail.com>
2018-08-10 14:45:40 -0600

    Merging

--- lab4_JosephMoscosoErickCarrasco/src/lab4_josephmoscosoerickcarrasco/Angeles.java
+++ lab4_JosephMoscosoErickCarrasco/src/lab4_josephmoscosoerickcarrasco/Angeles.java
@@ -5,10 +5,93 @@
  */
 package lab4_josephmoscosoerickcarrasco;
 
+import java.awt.Color;
+
 /**
  *
  * @author erick
  */
 public class Angeles {
+    private Color color;
+    private String descripcione;
+    private String colorsangre;
+    private String fecha;
+    private String nombre;
+    private String volar;
+    private int potencia;
+
+    public Angeles() {
+    }
+
+    public Angeles(Color color, String descripcione, String colorsangre, String fecha, String nombre, String volar, int potencia) {
+        this.color = color;
+        this.descripcione = descripcione;
+        this.colorsangre = colorsangre;
+        this.fecha = fecha;
+        this.nombre = nombre;
+        this.volar = volar;
+        this.potencia = potencia;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public String getDescripcione() {
+        return descripcione;
+    }
+
+    public void setDescripcione(String descripcione) {
+        this.descripcione = descripcione;
+    }
+
+    public String getColorsangre() {
+        return colorsangre;
+    }
+
+    public void setColorsangre(String colorsangre) {
+        this.colorsangre = colorsangre;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getVolar() {
+        return volar;
+    }
+
+    public void setVolar(String volar) {
+        this.volar = volar;
+    }
+
+    public int getPotencia() {
+        return potencia;
+    }
+
+    public void setPotencia(int potencia) {
+        this.potencia = potencia;
+    }
+
+    @Override
+    public String toString() {
+        return "Angeles{" + "color=" + color + ", descripcione=" + descripcione + ", colorsangre=" + colorsangre + ", fecha=" + fecha + ", nombre=" + nombre + ", volar=" + volar + ", potencia=" + potencia + '}';
+    }
     
 }
--- lab4_JosephMoscosoErickCarrasco/src/lab4_josephmoscosoerickcarrasco/EVA.java
+++ lab4_JosephMoscosoErickCarrasco/src/lab4_josephmoscosoerickcarrasco/EVA.java
@@ -1,9 +1,103 @@
 package lab4_josephmoscosoerickcarrasco;
 
+import java.awt.Color;
+
 /**
  *
  * @author erick
  */
 public class EVA {
+    private Color color;
+    private int productionDate;
+    private double altura;
+    private String alma;
+    private int ojos;
+    private String pais;
+    private int serie;
+    private double porcentajedamage;
+
+    public EVA() {
+    }
+
+    public EVA(Color color, int productionDate, double altura, String alma, int ojos, String pais, int serie, double porcentajedamage) {
+        this.color = color;
+        this.productionDate = productionDate;
+        this.altura = altura;
+        this.alma = alma;
+        this.ojos = ojos;
+        this.pais = pais;
+        this.serie = serie;
+        this.porcentajedamage = porcentajedamage;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public int getProductionDate() {
+        return productionDate;
+    }
+
+    public void setProductionDate(int productionDate) {
+        this.productionDate = productionDate;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public String getAlma() {
+        return alma;
+    }
+
+    public void setAlma(String alma) {
+        this.alma = alma;
+    }
+
+    public int getOjos() {
+        return ojos;
+    }
+
+    public void setOjos(int ojos) {
+        this.ojos = ojos;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public int getSerie() {
+        return serie;
+    }
+
+    public void setSerie(int serie) {
+        this.serie = serie;
+    }
+
+    public double getPorcentajedamage() {
+        return porcentajedamage;
+    }
+
+    public void setPorcentajedamage(double porcentajedamage) {
+        this.porcentajedamage = porcentajedamage;
+    }
+
+    @Override
+    public String toString() {
+        return "EVA{" + "color=" + color + ", productionDate=" + productionDate + ", altura=" + altura + ", alma=" + alma + ", ojos=" + ojos + ", pais=" + pais + ", serie=" + serie + ", porcentajedamage=" + porcentajedamage + '}';
+    }
+    
     
 }
