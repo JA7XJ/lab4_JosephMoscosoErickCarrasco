e3f3885b71173122f9371546991e95133eaea457
Your Name <josephmoscoso70@hotmail.com>
2018-08-10 19:42:00 -0600

    cosas

--- lab4_JosephMoscosoErickCarrasco/src/lab4_josephmoscosoerickcarrasco/Lab4_JosephMoscosoErickCarrasco.java
+++ lab4_JosephMoscosoErickCarrasco/src/lab4_josephmoscosoerickcarrasco/Lab4_JosephMoscosoErickCarrasco.java
@@ -31,37 +31,34 @@ public class Lab4_JosephMoscosoErickCarrasco {
         ev.add(new eva00(Color.darkGray, 2, 30, "ESHTA", 9, "Wakanda", 243, 21));
         ev.add(new eva00(Color.gray, 12, 50, "RODRIX", 10, "EL Salvador", 543, 34));
         ev.add(new eva02(Color.MAGENTA, 5, 30, "DIEGO", 15, "MALAZUELA", 432, 10));
-        ev.add(new  evapm(Color.WHITE, 11, 25, "Elmeromero", 20, "Gringolandia", 432,10));
-        ev.add(new  evapm(Color.CYAN, 30, 55, "CUACK CUACK", 19, "Chile", 502,40));
-        ev.add(new  evapm(Color.red, 4, 85, "ISIS", 20, "Gringolandia", 432,10));
-        ev.add(new  evapm(Color.yellow, 1, 49, "ARIEL", 20, "Elysium", 928,80));
-        
-        
-        
-        pil.add(new Piloto("Rocho", 34, "Transa","Joseph", "X-R Echelon", 65.3, ev.get(0)));
-        pil.add(new Piloto("Elmer", 37, "Chorro","Stale", "Pro-Alpha", 95.3, ev.get(1)));
-        pil.add(new Piloto("Fryden", 28, "Pejelagarto","Ed", "Archive type 3", 34.9, ev.get(2)));
-        pil.add(new Piloto("Joshua", 20, "Braulix","Suazzo", "PSM Elite", 88.7, ev.get(3)));
-        pil.add(new Piloto("Carl", 29, "Dane","Elixius", "RP Echo", 78.2, ev.get(4)));
-        pil.add(new Piloto("Props", 44, "Chicken","CJ", "Smokemouth", 31.8, ev.get(5)));
-        pil.add(new Piloto("Mord", 19, "Elias","XP", "X-R Echelon", 65.3, ev.get(6)));
-        pil.add(new Piloto("Rocho", 34, "Transa","Joseph", "X-R Echelon", 65.3, ev.get(7)));
-        
-        an.add(new ramiel(Color.CYAN, "Elemental", "Plateado", "12424.323 Deca Cycles", "Ay-WX","Vuela", 9000000 ));
-        an.add(new ramiel(Color.black, "Phazon-T1", "Blue-ish red", "2.82 Penta Cycles", "Kos","Vuela", 18000000 ));
-        an.add(new zeruel(Color.DARK_GRAY, "ICE SPEARS", "White gray", "49.7 Deca Cycles", "XR-Syron","No vuela", 60000 ));
-        an.add(new zeruel(Color.GREEN, "Mechanoid", "Black", "38.7 Deca Cycles", "EP-Hydra","Vuela", 8090000 ));
-        an.add(new zeruel(Color.blue, "Magnetic Aura", "cyan", "32 Nona Cycles", "CY- SMK","No vuela", 108200000 ));
-        an.add(new zeruel(Color.green, "Kinesis", "White purple", "102.7 Deca Cycles", "ELIXEX","No vuela", 9000 ));
-        an.add(new sachiel(Color.red, "Magma", "Dark Red", "43 Octa Cycles", "XT-Kryp","No vuela", 21460000 ));
-        an.add(new sachiel(Color.magenta, "Ultrafrigid Breath", "Red", "88 Hexa Cycles", "FT-Pryp","Vuela", 12460000 ));
-        
-        
+        ev.add(new evapm(Color.WHITE, 11, 25, "Elmeromero", 20, "Gringolandia", 432, 10));
+        ev.add(new evapm(Color.CYAN, 30, 55, "CUACK CUACK", 19, "Chile", 502, 40));
+        ev.add(new evapm(Color.red, 4, 85, "ISIS", 20, "Gringolandia", 432, 10));
+        ev.add(new evapm(Color.yellow, 1, 49, "ARIEL", 20, "Elysium", 928, 80));
+
+        pil.add(new Piloto("Rocho", 34, "Transa", "Joseph", "X-R Echelon", 65.3, ev.get(0)));
+        pil.add(new Piloto("Elmer", 37, "Chorro", "Stale", "Pro-Alpha", 95.3, ev.get(1)));
+        pil.add(new Piloto("Fryden", 28, "Pejelagarto", "Ed", "Archive type 3", 34.9, ev.get(2)));
+        pil.add(new Piloto("Joshua", 20, "Braulix", "Suazzo", "PSM Elite", 88.7, ev.get(3)));
+        pil.add(new Piloto("Carl", 29, "Dane", "Elixius", "RP Echo", 78.2, ev.get(4)));
+        pil.add(new Piloto("Props", 44, "Chicken", "CJ", "Smokemouth", 31.8, ev.get(5)));
+        pil.add(new Piloto("Mord", 19, "Elias", "XP", "X-R Echelon", 65.3, ev.get(6)));
+        pil.add(new Piloto("Rocho", 34, "Transa", "Joseph", "X-R Echelon", 65.3, ev.get(7)));
+
+        an.add(new ramiel(Color.CYAN, "Elemental", "Plateado", "12424.323 Deca Cycles", "Ay-WX", "Vuela", 9000000));
+        an.add(new ramiel(Color.black, "Phazon-T1", "Blue-ish red", "2.82 Penta Cycles", "Kos", "Vuela", 18000000));
+        an.add(new zeruel(Color.DARK_GRAY, "ICE SPEARS", "White gray", "49.7 Deca Cycles", "XR-Syron", "No vuela", 60000));
+        an.add(new zeruel(Color.GREEN, "Mechanoid", "Black", "38.7 Deca Cycles", "EP-Hydra", "Vuela", 8090000));
+        an.add(new zeruel(Color.blue, "Magnetic Aura", "cyan", "32 Nona Cycles", "CY- SMK", "No vuela", 108200000));
+        an.add(new zeruel(Color.green, "Kinesis", "White purple", "102.7 Deca Cycles", "ELIXEX", "No vuela", 9000));
+        an.add(new sachiel(Color.red, "Magma", "Dark Red", "43 Octa Cycles", "XT-Kryp", "No vuela", 21460000));
+        an.add(new sachiel(Color.magenta, "Ultrafrigid Breath", "Red", "88 Hexa Cycles", "FT-Pryp", "Vuela", 12460000));
+
         ev.get(0).setPiloto(pil.get(0));
         ev.get(1).setPiloto(pil.get(1));
         ev.get(2).setPiloto(pil.get(4));
         ev.get(3).setPiloto(pil.get(5));
-        
+
         System.out.println("Bienvenido a Nerv");
         System.out.println("------------------------------------------------------");
         System.out.println("............................-*@WWWWWWWWWWWW+...............\n"
@@ -117,13 +114,13 @@ public class Lab4_JosephMoscosoErickCarrasco {
         {" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
         {" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
         {" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "}};
-        
+
         Radar1 = asign(Radar1);
-        
+
         int c = 1;
         while (c == 1) {
-            int op=0;
-            
+            int op = 0;
+
             System.out.println("------------------------------------------------------");
             System.out.println("1-Lista de EVAs \n"
                     + "2-Lista de Angeles \n"
@@ -131,36 +128,36 @@ public class Lab4_JosephMoscosoErickCarrasco {
                     + "4-Simulación \n"
                     + "5-Salir");
             System.out.println("------------------------------------------------------");
-            int v= 1;
-            while (v==1) {
+            int v = 1;
+            while (v == 1) {
                 try {
                     System.out.print("Ingrese un numero correspondiente a la opcion: ");
                     op = sc.nextInt();
-                    v=2;
+                    v = 2;
                 } catch (InputMismatchException e) {
                     System.out.println("ERROR IN DATA");
-                    v=1;
+                    v = 1;
                 }
             }
             System.out.println("------------------------------------------------------");
             if (op == 1) {
                 System.out.println("------------------------------------------------------");
                 for (int i = 0; i < ev.size(); i++) {
-                    System.out.println(i+")"+ev.get(i));
+                    System.out.println(i + ")" + ev.get(i));
                 }
                 System.out.println("------------------------------------------------------");
             }
             if (op == 2) {
                 System.out.println("------------------------------------------------------");
                 for (int i = 0; i < an.size(); i++) {
-                    System.out.println(i+")"+an.get(i));
+                    System.out.println(i + ")" + an.get(i));
                 }
                 System.out.println("------------------------------------------------------");
             }
             if (op == 3) {
                 System.out.println("------------------------------------------------------");
                 for (int i = 0; i < pil.size(); i++) {
-                    System.out.println(i+")"+pil.get(i));
+                    System.out.println(i + ")" + pil.get(i));
                 }
                 System.out.println("------------------------------------------------------");
                 /*System.out.println("1-Listar\n"
@@ -192,15 +189,24 @@ public class Lab4_JosephMoscosoErickCarrasco {
                 //SIMULACION
                 int lives1 = 8;
                 int lives2 = 8;
-                boolean simulador=true;
+                boolean simulador = true;
                 int turn = 1;
-                do{
+                ImpMatrix(Radar1, 0, 0);
+                do {
                     if (turn == 1) {
                         System.out.println("User 1 turns");
-                        
+                        System.out.println("Ingrese x de una pieza: ");
+                        int x = sc.nextInt();
+                        System.out.println("Ingrese y de una pieza: ");
+                        int y = sc.nextInt();
+                        if ("PM".equals(Radar1[x][y])) {
+                            evapm m=new evapm();
+                            Radar1=m.movimiento(Radar1, 3, 3, x, y);
+                            ImpMatrix(Radar1, 0,0);
+                        }
                     }
-                    
-                }while(simulador);
+
+                } while (simulador);
             }
             if (op == 5) {
                 c = 0;
@@ -213,45 +219,45 @@ public class Lab4_JosephMoscosoErickCarrasco {
     }
 
     public static void inicializar() {
-        pil.add(new Piloto("Rocho", 34, "Transa","Joseph", "X-R Echelon", 65.3, ev.get(0)));
-        pil.add(new Piloto("Elmer", 37, "Chorro","Stale", "Pro-Alpha", 95.3, ev.get(1)));
-        pil.add(new Piloto("Fryden", 28, "Pejelagarto","Ed", "Archive type 3", 34.9, ev.get(2)));
-        pil.add(new Piloto("Joshua", 20, "Braulix","Suazzo", "PSM Elite", 88.7, ev.get(3)));
-        pil.add(new Piloto("Carl", 29, "Dane","Elixius", "RP Echo", 78.2, ev.get(4)));
-        pil.add(new Piloto("Props", 44, "Chicken","CJ", "Smokemouth", 31.8, ev.get(5)));
-        pil.add(new Piloto("Mord", 19, "Elias","XP", "X-R Echelon", 65.3, ev.get(6)));
-        pil.add(new Piloto("Rocho", 34, "Transa","Joseph", "X-R Echelon", 65.3, ev.get(7)));
-        pil.add(new Piloto("Rocho", 34, "Transa","Joseph", "X-R Echelon", 65.3, ev.get(8)));
+        pil.add(new Piloto("Rocho", 34, "Transa", "Joseph", "X-R Echelon", 65.3, ev.get(0)));
+        pil.add(new Piloto("Elmer", 37, "Chorro", "Stale", "Pro-Alpha", 95.3, ev.get(1)));
+        pil.add(new Piloto("Fryden", 28, "Pejelagarto", "Ed", "Archive type 3", 34.9, ev.get(2)));
+        pil.add(new Piloto("Joshua", 20, "Braulix", "Suazzo", "PSM Elite", 88.7, ev.get(3)));
+        pil.add(new Piloto("Carl", 29, "Dane", "Elixius", "RP Echo", 78.2, ev.get(4)));
+        pil.add(new Piloto("Props", 44, "Chicken", "CJ", "Smokemouth", 31.8, ev.get(5)));
+        pil.add(new Piloto("Mord", 19, "Elias", "XP", "X-R Echelon", 65.3, ev.get(6)));
+        pil.add(new Piloto("Rocho", 34, "Transa", "Joseph", "X-R Echelon", 65.3, ev.get(7)));
+        pil.add(new Piloto("Rocho", 34, "Transa", "Joseph", "X-R Echelon", 65.3, ev.get(8)));
     }
-    
-    public static void ImpMatrix(String Matriz[][], int filas, int columnas){
-        
-        if (filas== Matriz.length-1 && columnas == Matriz[0].length-1) {
-            System.out.println("[ "+Matriz[filas][columnas]+ " ]");
-            
+
+    public static void ImpMatrix(String Matriz[][], int filas, int columnas) {
+
+        if (filas == Matriz.length - 1 && columnas == Matriz[0].length - 1) {
+            System.out.println("[ " + Matriz[filas][columnas] + " ]");
+
         } else {
-            if (columnas == Matriz[0].length-1) {
-                System.out.println("[ "+Matriz[filas][columnas]+ " ]");
-                ImpMatrix(Matriz,filas+1,0);
+            if (columnas == Matriz[0].length - 1) {
+                System.out.println("[ " + Matriz[filas][columnas] + " ]");
+                ImpMatrix(Matriz, filas + 1, 0);
             } else {
-                System.out.print("[ "+Matriz[filas][columnas]+ " ]");
-                ImpMatrix(Matriz, filas, columnas +1);
+                System.out.print("[ " + Matriz[filas][columnas] + " ]");
+                ImpMatrix(Matriz, filas, columnas + 1);
             }
         }
     }
-    
-    public static String [][] asign(String[][]radar){
-        
+
+    public static String[][] asign(String[][] radar) {
+
         //
-        radar [0][2] = "R";
-        radar [0][7] = "R";
-        radar [1][1] = "Z";
-        radar [1][8] = "Z";
-        radar [1][4] = "S";
-        radar [1][5] = "S";
-        radar [3][2] = "Z";
-        radar [3][7] = "Z";
-        
+        radar[0][2] = "R";
+        radar[0][7] = "R";
+        radar[1][1] = "Z";
+        radar[1][8] = "Z";
+        radar[1][4] = "S";
+        radar[1][5] = "S";
+        radar[3][2] = "Z";
+        radar[3][7] = "Z";
+
         radar[9][0] = "PM";
         radar[9][2] = "PM";
         radar[9][3] = "EV0";
@@ -260,9 +266,8 @@ public class Lab4_JosephMoscosoErickCarrasco {
         radar[9][6] = "EV0";
         radar[9][7] = "PM";
         radar[9][9] = "PM";
-        
+
         return radar;
     }
-
 
 }
